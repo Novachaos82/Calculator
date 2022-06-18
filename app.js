33e1e99e8b824c7a5fe9bc55142e45d502b3cd25
Bhavesh Dewangan <ybkdddd@gmail.com>
2022-06-18 14:13:48 +0530

    will add Keyb support

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let operator = "x";
+let operator = "";
 
 const prevDisplay = document.querySelector(".previous-display");
 const currDisplay = document.querySelector(".current-display");
@@ -9,15 +9,20 @@ const operators = document.querySelectorAll(".operator");
 const numbers = document.querySelectorAll(".number");
 const equal = document.querySelector(".equals");
 
-//function currentDisplay()
-//{
-//    currDisplay = ;
-//}
-
 /*all clear button*/
 allClear.addEventListener("click", () => {
   prevDisplay.innerHTML = "";
   currDisplay.innerHTML = "";
+  operator = "";
+});
+
+clear.addEventListener("click", () => {
+  currDisplay.innerText = currDisplay.innerText.slice(0, -1);
+  console.log("yes");
+});
+
+equal.addEventListener("click", () => {
+  calculate(operator);
 });
 
 /*taking numbers input in current display*/
@@ -30,43 +35,83 @@ numbers.forEach((number) => {
   });
 });
 
-equal.addEventListener("click", () => {
-  calculate(operator);
-});
-
 operators.forEach((operation) => {
   operation.addEventListener("click", () => {
     //console.log(operation.innerText);
-    if (operation.innerText === "x") {
-      prevDisplay.innerText += currDisplay.innerText.toString() + "x";
-      currDisplay.innerText = "";
-      operator = operation.innerText;
+    if (currDisplay.innerText != "") {
+      if (operation.innerText === "x") {
+        prevDisplay.innerText = currDisplay.innerText.toString() + "x";
+        currDisplay.innerText = "";
+        operator = operation.innerText;
+      }
+      if (operation.innerText === "+") {
+        prevDisplay.innerText = currDisplay.innerText.toString() + "+";
+        currDisplay.innerText = "";
+        operator = operation.innerText;
+      }
+      if (operation.innerText === "-") {
+        prevDisplay.innerText = currDisplay.innerText.toString() + "-";
+        currDisplay.innerText = "";
+        operator = operation.innerText;
+      }
+      if (operation.innerText === "÷") {
+        prevDisplay.innerText = currDisplay.innerText.toString() + "÷";
+        currDisplay.innerText = "";
+        operator = operation.innerText;
+      }
     }
   });
 });
 
 function calculate(op) {
   if (op === "x") {
-    const result = float(prevDisplay.innerText) * float(currDisplay.innerText);
+    const result =
+      parseFloat(prevDisplay.innerText) * parseFloat(currDisplay.innerText);
     currDisplay.innerText = result;
+    prevDisplay.innerText = "";
   }
 
   if (op === "+") {
+    const result =
+      parseFloat(prevDisplay.innerText) + parseFloat(currDisplay.innerText);
+    currDisplay.innerText = result;
+    prevDisplay.innerText = "";
   }
 
   if (op === "-") {
+    const result =
+      parseFloat(prevDisplay.innerText) - parseFloat(currDisplay.innerText);
+    currDisplay.innerText = result;
+    prevDisplay.innerText = "";
   }
 
   if (op === "÷") {
+    const result =
+      parseFloat(prevDisplay.innerText) / parseFloat(currDisplay.innerText);
+    if (currDisplay.innerText == 0) {
+      alert("error");
+    }
+    currDisplay.innerText = result;
+    prevDisplay.innerText = "";
   }
 }
 
 function display() {
-  currDisplay.innerText = 0;
-  prevDisplay.innerText = 0;
+  currDisplay.innerText = "";
+  prevDisplay.innerText = "";
 }
 
+//document.addEventListener("keydown", (e) => {
+//  if ((e.key <= 9 && e.key >= 0) || (e.key = ".")) {
+//    currDisplay.innerText = currDisplay.innerText.toString() + e.key.toString();
+//    if (e.key === "*" || e.key === "-" || e.key === "+" || e.key === "/") {
+//      {
+//        calculate(e.key);
+//      }
+//    }
+//  }
+//});
+
 document.addEventListener("DOMContentLoaded", () => {
-  calculate();
   display();
 });
